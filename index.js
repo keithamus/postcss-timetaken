8dbc8660fd9043bf5804e21fce54d89cc5962ced
Keith Cirkel <keithamus@users.noreply.github.com>
2018-09-27 13:03:59 +0100

    initial commit

---
+++ index.js
@@ -0,0 +1,23 @@
+const {plugin} = require('postcss')
+const {relative} = require('path')
+const {readFileSync, writeFileSync} = require('fs')
+module.exports = plugin('postcss-timings', function (opts) {
+  opts.file = opts.file || 'timings'
+  console.log(opts.file)
+  const start = Date.now()
+  return (root, result) => {
+    const path = relative(process.cwd(), result.opts.from)
+    let timings
+    try {
+      timings = readFileSync(opts.file, 'utf-8')
+        .split('\n')
+        .map(line => line.split(' '))
+        .filter(([time, file]) => file !== path)
+    } catch (e) {
+      timings = []
+    }
+    timings.push([Date.now() - start, path])
+    timings = timings.map(line => line.join(' ')).join('\n')
+    writeFileSync(opts.file, timings, 'utf-8')
+  }
+})
